fec95ff47e82f8e658422771fd5963982d822a86
Kifansa <150925814+Kifansa@users.noreply.github.com>
2024-12-13 21:25:12 +0700

    Add files via upload

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ $pageTitle }}</title>
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss'])
+</head>
+<body>
+    @include('layouts.nav')
+    <div id="app">
+        <main class="py-4">
+            @yield('content')
+        </main>
+        @vite('resources/js/app.js')
+    </div>
+</body>
+</html>
